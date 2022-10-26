e15c04ed687b91f11cffa3b8f95531433777c1e9
Zhadyra Oralkhanova <oralkhanova.zhadyra@gmail.com>
2022-10-26 15:54:18 +0600

    feat: products implementation

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+import {IProduct} from "../models";
 
-export function Product(props: any) {
+interface ProductProps {
+    product: IProduct
+}
+export function Product({product}: ProductProps) {
+    const [ details, setDetails ] = useState(false);
+
+    const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400';
+    const btnClasses = ['py-2 px-4 border', btnBgClassName]
     return(
-        <div>Product</div>
+        <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+            <img className="w-1/6" src={product.image} alt={product.title}/>
+            <p>{product.title}</p>
+            <p className="font-bold">{product.price}</p>
+            <button
+                onClick={() => {setDetails(prev => !prev)}}
+                className={btnClasses.join(' ')}>{ details ? 'Hide Details' : 'Show Details'}</button>
+            { details &&
+                <div>
+                    <p>{product.description}</p>
+                    <p>Rate: <span style={{ fontWeight: 'bold'}}>{product.rating.rate}</span></p>
+                </div>
+            }
+        </div>
     )
 }
